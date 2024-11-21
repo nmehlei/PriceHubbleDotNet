65652e31cb1c10981355d11b2bc43123ff3003d6
Nicolas Mehlei <pharao2k@hotmail.com>
2024-11-21 21:41:49 +0100

    added range property to Valuation value type

--- src/PriceHubble.Client/ValueTypes/Valuation.cs
+++ src/PriceHubble.Client/ValueTypes/Valuation.cs
@@ -8,12 +8,19 @@ namespace PriceHubble.Client.ValueTypes
         public int Value { get; set; }
 
         /// <summary>
+        /// Estimated lower/upper bound of the property's sale price / net rent / gross rent value
+        /// </summary>
+        public ValueRange ValueRange { get; set; }
+
+        /// <summary>
         /// The date of the valuation
+        /// YYYY-MM-DD
         /// </summary>
         public DateTime ValuationDate { get; set; }
 
         /// <summary>
         /// Confidence of the valuation
+        /// Valid values are poor, medium or good
         /// </summary>
         public Confidence ValuationConfidence { get; set; }
     }
